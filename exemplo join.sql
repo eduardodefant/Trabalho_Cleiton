25660eb310aaf691c96f27f9464745f6330b8307
eduardodefant <e.c.d-@hotmail.com>
2017-05-11 21:57:06 -0300

    fgh
    hgfhgf

---
+++ exemplo join.sql
@@ -0,0 +1,23 @@
+﻿insert into professor
+(nome, cpf)
+values
+('Rodrigo', '05865865421')
+
+
+select * from professor
+
+insert into disciplina
+(nome,curso)
+values
+('Banco de dados II', 'Sistemas de Informação')
+
+select * from disciplina
+
+insert into professor_disciplina
+(iddisciplina, idprofessor)
+values
+(3,2)
+
+select * from professor_disciplina
+join professor 
+on(professor_disciplina.idprofessor = professor.idprofessor)
